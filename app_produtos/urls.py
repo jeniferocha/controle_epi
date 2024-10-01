409128ae55651faf0bdfc0ccadd6305e080a326f
jeniferocha <jenifermartinsdarocha@hotmail.com>
2024-10-01 14:22:05 -0300

    inclusao filtro nome equipamento

--- app_produtos/urls.py
+++ app_produtos/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index), 
-    path("cadastrarEquipamento/", views.cadastrarEquipamento, name="cadastrarEquipamento"), 
-    path("editarEquipamento/", views.editarEquipamento, name="editarEquipamento"),
+    path("cadastrarEquipamento/", views.cadastrar_equipamento, name="cadastrar_equipamento"), 
+    path("listarEquipamento/", views.listar_equipamento, name="listar_equipamento"),
+    path("editarEquipamento/<int:id>", views.editar_equipamento, name="editar_equipamento"),
+    path("deletarEquipamento/<int:id>", views.deletar_equipamento, name='deletar_equipamento'), 
+
  
 ]
--- app_produtos/views.py
+++ app_produtos/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import CadastrarEquipamento
 
 def index(request):
     return render(request, 'app_produtos/globals/home.html')
 
-def cadastrarEquipamento(request):
+
+def listar_equipamento(request):
+    values = CadastrarEquipamento.objects.all()
+    cod_equipamento = request.GET.get('cod_equipamento')    
+    nome_equipamento = request.GET.get('nome_equipamento')
+    if nome_equipamento:
+        values = values.filter(nome_equipamento__icontains=nome_equipamento)
+
+    return render(request, 'app_produtos/globals/listarEquipamento.html', {
+        "lista_equipamento": values,
+        "nome_equipamento": nome_equipamento
+    })
+
+
+def cadastrar_equipamento(request):
     equipamento = {}
     if request.method == 'POST':
         cod_equipamento = request.POST.get('cod_equipamento')    
@@ -18,5 +32,24 @@ def cadastrarEquipamento(request):
 
     return render(request, 'app_produtos/globals/cadastrarEquipamento.html', {"equipamento":equipamento})
 
-def editarEquipamento(request):
-    return render(request, 'app_produtos/globals/editarEquipamento.html')
+
+
+def editar_equipamento(request, id):
+    equipamento = CadastrarEquipamento.objects.get(id=id)
+    if request.method == 'POST':
+        cod_equipamento = request.POST.get('cod_equipamento')    
+        nome_equipamento = request.POST.get('nome_equipamento') 
+        if cod_equipamento and nome_equipamento:
+            equipamento.nome_equipamento = nome_equipamento
+            equipamento.cod_equipamento = cod_equipamento
+            equipamento.save()
+            return redirect(listar_equipamento)  
+        else:
+            return render(request, 'app_produtos/globals/editarEquipamento.html', {"item":equipamento, "erro":True})
+    return render(request, 'app_produtos/globals/editarEquipamento.html', {"item":equipamento})   
+
+
+def deletar_equipamento(request, id):
+    equipamento = CadastrarEquipamento.objects.get(id=id)
+    equipamento.delete()
+    return redirect(listar_equipamento)  
